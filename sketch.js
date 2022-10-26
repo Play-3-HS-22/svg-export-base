0434e7e48041affab6093ef605818b7367c991a7
chrischne <chrischne@gmail.com>
2022-10-26 16:55:52 +0200

    asdf

--- sketch.js
+++ sketch.js
@@ -8,30 +8,45 @@ var projection = d3
   .scale(300000)
 
 
+var colScale = d3
+  .scaleSequential(d3.interpolateSpectral)
+  .domain([0,5]);
+
+
 function preload() {
   benches = loadJSON('benches.json')
 }
 
 function setup() {
-  createCanvas(800, 800,SVG);
+  createCanvas(800, 800);
 
   console.log(benches);
 
+
+  console.log(colScale(0));
+  console.log(colScale(1));
+  console.log(colScale(2));
+  console.log(colScale(4));
+ 
   noLoop();
 }
 
 function draw() {
-  clear();
+  background(255);
 
   // Draw benches looping over benches.features
   for (let i = 0; i < benches.features.length; i++) {
     let bench = benches.features[i]
     let pos = projection(bench.geometry.coordinates)
+
+
+    let col = colScale(bench.properties.FARBE);
+    console.log(col);
     noStroke();
-    fill(255,0,0)
+    fill(col)
     rect(pos[0], pos[1], 10, 3)
   }
 
-  save("benches.svg");
+  
 
 }
